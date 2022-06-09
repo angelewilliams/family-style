42327f8beb8edc6b242062bd5522479ecf3338e3
Angele Williams <angele.williams5@gmail.com>
2022-06-08 23:09:59 -0600

    Start component structure and api calls

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,25 +1,32 @@
 import React, { useState, useEffect } from 'react'
-import { Route, NavLink } from 'react-router-dom';
-
+import { Route, NavLink, Switch } from 'react-router-dom';
+import { fetchRecipes } from './../../apiCalls'
+import Recipes from './../Recipes/Recipes'
 
 // import logo from './../Images/logo.svg';
 import './App.css';
 
-App = () => {
+const App = () => {
+  const [group, setGroup] = useState('williams');
+  const [recipes, setRecipes] = useState([]);
+
+
+  useEffect(() => {
+    fetchRecipes()
+    .then(recipes => console.log(recipes))
+    .then(console.log('something may have worked'))
+  }, []);
+
   return (
     <body className="App">
       <header className="App-header">
-        <NavLink to="/about">About</NavLink>
-        <NavLink to="/favorites">Favorites</NavLink>
-        <NavLink to="/recipeLinks">Recipes</NavLink>
+      
         <img src='broken.link' className="App-logo" alt="logo" />
       </header>
       <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
+
         <Route exact path="/:groupId/recipeLinks">
-          <RecipeLinks />
+          <Recipes recipesProps='williams'/>
         </Route>
       </Switch>
 
@@ -32,6 +39,15 @@ App = () => {
 
 export default App;
 
+//CODE HOARDING AREA
 // <Route exact path="/">
 //   {loggedIn ? <Redirect to="/dashboard" /> : <PublicHomePage />}
 // </Route>
+
+// <NavLink to="/about">About</NavLink>
+// <NavLink to="/favorites">Favorites</NavLink>
+// <NavLink to="/recipeLinks">Recipes</NavLink>
+
+{/* <Route exact path="/">
+<LandingPage />
+</Route> */}
---
+++ src/Components/RecipeLink/RecipeLink.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import './RecipeLink.css'
+
+const RecipeLink = () => {
+
+    return(
+        <article className='recipe-link'>
+            <h2>this is a recipe title</h2>
+        </article>
+    )
+}
+
+
+export default RecipeLink
